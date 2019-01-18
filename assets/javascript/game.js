985b5de8840e55c84a9dd39d2c70bc190bb783e0
isai <Isai4827@4827@gmail.com>
2019-01-18 10:02:01 -0700

    add assets/ index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
 var wins = 0;
 var losses = 0;
+var computerChoices ="a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,"
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
-    if ((userGuess === "p") && (computerGuess === "s")) {
-        losses++;
-    }
-    if ((userGuess === "r") && (computerGuess === "s")) {
+    if ((userGuess === event.key) && (computerGuess === event.key)) {
         wins++;
     }
+    if ((userGuess === event.key) && (computerGuess === event.key)) {
+        losses++;
+    }
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
 }
